762268135740833eb5d72da0d8d46e491be03570
Rodrigo <auxrodrigoblancoh8@gmail.com>
2023-11-09 14:19:40 -0400

    libro de cocina

---
+++ README.md
@@ -0,0 +1,13 @@
+# FlowGPT 
+
+This is Docs site for [**FlowGPT**](https://flowgpt.com).
+
+## Local Development
+
+First, run `yarn install` to install the dependencies.
+
+Then, run `yarn dev` to start the development server and visit localhost:3000.
+
+You can also use npm if you want.
+
+
---
+++ theme.config.tsx
@@ -0,0 +1,69 @@
+//I used nextra library for next.js. This is theme configuration for it:
+//https://nextra.site/docs/docs-theme/theme-configuration
+
+import React from "react";
+
+const config = {
+  logo: (
+    <>
+    
+     <img
+       src="/favicon.ico"
+        alt="FlowGPT"
+        style={{ width: "24px", height: "24px", marginRight: "8px" }}
+      />
+    <span >
+     
+      <b>FlowGPT</b> Cookbook
+    </span></>
+  ),
+
+  chat: {
+    link: "https://discord.gg/flowgpt",
+  },
+
+   project: {
+    link: 'https://github.com/FlowGPT/guide'
+  },
+
+  footer: {
+    text: "FlowGPT",
+  },
+
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – FlowGPT",
+    };
+  },
+
+  nextThemes: {
+    defaultTheme: "system",
+  },
+
+  // banner: {
+  //   key: "hackathon",
+  //   text: (
+  //     <a href="https://flowgpt.com/bounty" target="_blank">
+  //       🎉 FlowGPT Hackathon Season 2 is released. Join here →
+  //     </a>
+  //   ),
+  // },
+
+  feedback: {
+    content: null
+  },
+
+  editLink: {
+    text: null
+  },
+
+
+
+
+
+
+
+   
+};
+
+export default config;
